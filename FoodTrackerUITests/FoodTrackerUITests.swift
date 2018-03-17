ad5bfcd86b905e1a02aa28bed4b256efcba034b6
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-03-17 08:15:30 -0400

    adding ui test module

---
+++ FoodTrackerUITests/FoodTrackerUITests.swift
@@ -0,0 +1,29 @@
+//
+//  FoodTrackerUITests.swift
+//  FoodTrackerUITests
+//
+//  Created by Wilson Campusano on 3/17/18.
+//  Copyright © 2018 Wilson Campusano. All rights reserved.
+//
+
+import XCTest
+
+class FoodTrackerUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+	
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+
+        super.tearDown()
+    }
+    
+    func testExample() {
+		
+    }
+    
+}
