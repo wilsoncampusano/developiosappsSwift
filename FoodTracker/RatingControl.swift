1da63f46fb1a20536d6ca24d40fc594c8e5304f5
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-03-18 14:24:27 -0400

    added custom control class and stackview

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,23 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Wilson Campusano on 3/18/18.
+//  Copyright © 2018 Wilson Campusano. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+	
+	//MARK: initialization
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	
+	required init(coder: NSCoder) {
+		super.init(coder: coder)
+	}
+}
