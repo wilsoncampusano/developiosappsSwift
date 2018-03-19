8e551dfee426aadfaf4a73f1c3ca2e5d4b974cc1
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-03-19 11:15:03 -0400

    rating images

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -48,14 +48,18 @@ import UIKit
 	
 	private func setupButtons(){
 		removeButtons()
+		let images = loadButtonImages()
+		
 		for _ in 0..<starCount{
 			
 			let button = UIButton()
-			button.backgroundColor = .red
+			//button.backgroundColor = .red
 			button.translatesAutoresizingMaskIntoConstraints = false
 			button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
 			button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
-			
+			button.setImage(images.empty, for: .normal)
+			button.setImage(images.filled, for: .selected)
+			button.setImage(images.highligh, for: [.highlighted, .selected])
 			button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
 			
 			// this is not necesary because buttons were removed on initializers
@@ -64,9 +68,15 @@ import UIKit
 		}
 	}
 	
+	private func loadButtonImages() -> (filled: UIImage,empty: UIImage, highligh: UIImage){
+		//let bundle = Bundle(for: type(of:self))
+		return (#imageLiteral(resourceName: "filledStar"), #imageLiteral(resourceName: "emptyStar"), #imageLiteral(resourceName: "highlightedStar"))
+	}
+	
 	private func removeButtons(){
 		for b in buttons{
 			removeArrangedSubview(b)
+			b.removeFromSuperview()
 		}
 		buttons.removeAll()
 	}
