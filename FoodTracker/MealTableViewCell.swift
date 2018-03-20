d17eb0c6d1bfa92f0ded917fe2ff7fbb31238687
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-03-20 11:01:21 -0400

    designed cell ui. but not showing up

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Wilson Campusano on 3/20/18.
+//  Copyright © 2018 Wilson Campusano. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
