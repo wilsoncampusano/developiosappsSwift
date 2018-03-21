5a5f51a5cd0834acc61523a202b5e66d53626a1d
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-03-21 15:08:47 -0400

    implementing enconding y decoding on meal

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,11 +7,33 @@
 //
 import UIKit
 
-public class Meal {
+public class Meal : NSObject, NSCoding {
+	
 	var name:String
 	var photo: UIImage?
 	var rating: Int
 	
+	enum PropertyKey: String {
+		case name, photo, rating
+	}
+	
+	public func encode(with aCoder: NSCoder) {
+		aCoder.encode(self.name, forKey: PropertyKey.name.rawValue)
+		aCoder.encode(self.photo, forKey: PropertyKey.photo.rawValue)
+		aCoder.encode(self.rating, forKey: PropertyKey.rating.rawValue)
+	}
+	
+	
+	public required convenience init?(coder aDecoder: NSCoder) {
+		guard let name = aDecoder.decodeObject(forKey: PropertyKey.name.rawValue) as? String else {
+				fatalError("cannot decode name")
+		}
+		let photo = aDecoder.decodeObject(forKey: PropertyKey.photo.rawValue) as? UIImage
+		let rating =  aDecoder.decodeInteger(forKey: PropertyKey.rating.rawValue)
+		
+		self.init(name: name, photo: photo, rating: rating)
+	}
+
 	init?(name: String, photo: UIImage?, rating: Int){
 		guard !name.isEmpty,  !(rating < 0), (rating >= 0 && rating <= 5) else {
 			return nil
